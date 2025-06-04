002313b4f4311c8d405bd7e83a0d11c524734275
z3300 <zeeshanchatur@gmail.com>
2025-06-04 15:36:14 -0500

    added ssl config

--- src/server/db.ts
+++ src/server/db.ts
@@ -5,5 +5,11 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-export const prisma: PrismaClient = globalThis.prisma ?? new PrismaClient();
+export const prisma: PrismaClient = globalThis.prisma ?? new PrismaClient({
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma; 
