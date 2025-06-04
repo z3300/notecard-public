1b09b84cfbfe4549263db9aa6bec2430064e1ae1
z3300 <zeeshanchatur@gmail.com>
2025-06-04 15:03:34 -0500

    fixed build even more

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mycard-screenshots.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
